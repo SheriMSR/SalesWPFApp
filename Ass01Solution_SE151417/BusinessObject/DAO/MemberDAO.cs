49fc0c052a04a459298e8f69869760df02921059
SheriMSR <92568887+SheriMSR@users.noreply.github.com>
2022-09-05 22:01:24 +0700

    CRUD member + interface

---
+++ Ass01Solution_SE151417/BusinessObject/DAO/MemberDAO.cs
@@ -0,0 +1,127 @@
+﻿using DataAccess.DataAccess;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObject.DAO
+{
+    public class MemberDAO
+    {
+        private static MemberDAO instance = null;
+        private static readonly object instanceLock = new object();
+        private MemberDAO() { }
+        public static MemberDAO Instance
+        {
+            get
+            {
+                lock (instanceLock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new MemberDAO();
+                    }
+                    return instance;
+                }
+            }
+        }
+        
+        public IEnumerable<Member> GetMemberList()
+        {
+            List<Member> members;
+            try
+            {
+                var fStoreDB = new FStoreDBContext();
+                members = fStoreDB.Members.ToList();
+            }
+            catch(Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return members;
+        }
+
+        public Member GetMemberByID(int memberID)
+        {
+            Member member = null;
+            try
+            {
+                var fStoreDB = new FStoreDBContext();
+                member = fStoreDB.Members.SingleOrDefault(member => member.MemberId == memberID);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return member;
+        }
+
+        public void AddNew (Member member)
+        {
+            try
+            {
+                Member _member = GetMemberByID(member.MemberId);
+                if(_member == null)
+                {
+                    var fStoreDB = new FStoreDBContext();
+                    fStoreDB.Members.Add(member);
+                    fStoreDB.SaveChanges();
+                }
+                else
+                {
+                    throw new Exception("Member is already exist.");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public void Update(Member member)
+        {
+            try
+            {
+                Member m = GetMemberByID(member.MemberId);
+                if(m != null)
+                {
+                    var fStoreDB = new FStoreDBContext();
+                    fStoreDB.Entry<Member>(member).State = EntityState.Modified;
+                    fStoreDB.SaveChanges ();
+                }
+                else
+                {
+                    throw new Exception("Member does not exist.");
+                }
+            }
+            catch(Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public void Remove(Member member)
+        {
+            try
+            {
+                Member _member = GetMemberByID(member.MemberId);
+                if (_member != null)
+                {
+                    var fStoreDB = new FStoreDBContext();
+                    fStoreDB.Members.Remove(member);
+                    fStoreDB.SaveChanges();
+                }
+                else
+                {
+                    throw new Exception("Member does not exist.");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
---
+++ Ass01Solution_SE151417/BusinessObject/MemberRepository.cs
@@ -0,0 +1,32 @@
+﻿using BusinessObject.DAO;
+using DataAccess.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObject
+{
+    public interface IMemberRepository
+    {
+        IEnumerable<Member> GetMember();
+        Member GetMemberByID(int id);
+        void InsertMember(Member member);
+        void DeleteMember(Member member);
+        void UpdateMember(Member member);
+
+    }
+    public class MemberRepository : IMemberRepository
+    {
+        public void DeleteMember(Member member) => MemberDAO.Instance.Remove(member);
+
+        public IEnumerable<Member> GetMember() => MemberDAO.Instance.GetMemberList();
+
+        public Member GetMemberByID(int id) => MemberDAO.Instance.GetMemberByID(id);
+
+        public void InsertMember(Member member) => MemberDAO.Instance.AddNew(member);
+
+        public void UpdateMember(Member member) => MemberDAO.Instance.Update(member);
+    }
+}
