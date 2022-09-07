2f0eb3dac3cf6c3d75852686a748494927189df7
SheriMSR <92568887+SheriMSR@users.noreply.github.com>
2022-09-07 12:56:21 +0700

    OrderDetail CRUD + repository

---
+++ Ass01Solution_SE151417/BusinessObject/OrderDetailRepository.cs
@@ -0,0 +1,28 @@
+﻿using BusinessObject.DAO;
+using DataAccess.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObject
+{
+    public interface IOrderDetailRepository
+    {
+        OrderDetail GetOrderDetailByOrderID(int id);
+        void InsertOrderDetail(OrderDetail orderDetail);
+        void DeleteOrderDetail(OrderDetail orderDetail);
+        void UpdateOrderDetail(OrderDetail orderDetail);
+    }
+    public class OrderDetailRepository : IOrderDetailRepository
+    {
+        public void DeleteOrderDetail(OrderDetail orderDetail) => OrderDetailDAO.Instance.Remove(orderDetail);
+
+        public OrderDetail GetOrderDetailByOrderID(int id) => OrderDetailDAO.Instance.GetOrderDetailByOrderID(id);
+
+        public void InsertOrderDetail(OrderDetail orderDetail) => OrderDetailDAO.Instance.AddNew(orderDetail);
+
+        public void UpdateOrderDetail(OrderDetail orderDetail) => OrderDetailDAO.Instance.Update(orderDetail);
+    }
+}
