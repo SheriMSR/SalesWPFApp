2599c6808c9053e4d975258277cbc0da2f48cda0
SheriMSR <92568887+SheriMSR@users.noreply.github.com>
2022-09-08 16:53:27 +0700

    UI Window product + setup other Window + some stuff

---
+++ Ass01Solution_SE151417/SalesWPFApp/WindowProducts.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SalesWPFApp
+{
+    /// <summary>
+    /// Interaction logic for WindowProducts.xaml
+    /// </summary>
+    public partial class WindowProducts : Window
+    {
+        public WindowProducts()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnSubmit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnLoad_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnUpdate_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
