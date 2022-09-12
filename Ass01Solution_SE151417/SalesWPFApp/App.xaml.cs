3c98a3337a9d6dab0c3c863d1b9a08fa5340e09f
SheriMSR <92568887+SheriMSR@users.noreply.github.com>
2022-09-12 15:38:10 +0700

    nothing

--- Ass01Solution_SE151417/SalesWPFApp/App.xaml.cs
+++ Ass01Solution_SE151417/SalesWPFApp/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DataAccess;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +15,37 @@ namespace SalesWPFApp
     /// </summary>
     public partial class App : Application
     {
+        private ServiceProvider serviceProvider;
+        public App()
+        {
+            ServiceCollection services = new ServiceCollection();
+            ConfigureServices(services);
+            serviceProvider = services.BuildServiceProvider();
+        }
+
+        private void ConfigureServices(ServiceCollection services)
+        {
+            services.AddSingleton(typeof(IMemberRepository), typeof(MemberRepository));
+            services.AddSingleton<WindowMembers>();
+
+            services.AddSingleton(typeof(IOrderRepository), typeof(OrderRepository));
+            services.AddSingleton<WindowOrders>();
+
+            services.AddSingleton(typeof(IProductRepository), typeof(ProductRepository));
+            services.AddSingleton<WindowProducts>();
+        }
+
+        private void OnStartup(object sender, StartupEventArgs e)
+        {
+            var windowMember = serviceProvider.GetService<WindowMembers>();
+            windowMember.Show();
+
+            var windowOrder = serviceProvider.GetService<WindowOrders>();
+            windowOrder.Show();
+
+            var windowProduct = serviceProvider.GetService<WindowProducts>();
+            windowProduct.Show();
+
+        }
     }
 }
